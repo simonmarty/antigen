d2f234ff67a6f0f6d4e3b38a654a53a93b208179
Simon Marty <simon.marty0@gmail.com>
2024-11-21 04:20:11 +0000

    Resolve script file name at runtime

--- src/antigen.zsh
+++ src/antigen.zsh
@@ -1,4 +1,5 @@
 [[ -z "$_ANTIGEN_INSTALL_DIR" ]] && _ANTIGEN_INSTALL_DIR=${0:A:h}
+[[ -z "$_ANTIGEN_INSTALL_LOCATION" ]] && _ANTIGEN_INSTALL_LOCATION=${0:A}
 
 # Each line in this string has the following entries separated by a space
 # character.
